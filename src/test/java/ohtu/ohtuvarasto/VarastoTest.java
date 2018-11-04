f9ee023b599ce55cbc57238c122ddf905ab4221d
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2018-11-04 17:46:31 +0200

    Testi muutettu takaisin läpimeneväksi

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -143,7 +143,7 @@ public class VarastoTest {
     
     @Test
     public void varastonTulostusToimii() {
-        assertEquals("saldo = 0, vielä tilaa 10.0", varasto.toString());
+        assertEquals("saldo = 0.0, vielä tilaa 10.0", varasto.toString());
     }
 
 }
